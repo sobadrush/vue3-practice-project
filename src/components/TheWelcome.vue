77761a349cd91edcd0f68b80a1dd0a21c054e184
Roger Lo <sobadrush@icloud.com>
2024-07-27 00:48:20 +0800

    整理code

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -86,3 +86,5 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
 </template>
+
+
