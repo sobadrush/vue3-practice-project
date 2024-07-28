0b7572d5574b789ab6051d356b9ac64449ab89ba
Roger Lo <sobadrush@icloud.com>
2024-07-28 23:33:36 +0800

    about 頁面新增： 1. 圖片 2. icons

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,26 @@
 <template>
+  
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+
+  <CommunityIcon />
+  <DocumentationIcon />
+  <ToolingIcon />
+  <EcosystemIcon />
+  <SupportIcon />
+
+  <br/>
+  <img src="@/assets/images/DeadPool.jpeg"/>
+
 </template>
 
 <script setup lang="ts">
-
+  import CommunityIcon from '@/components/icons/IconCommunity.vue'
+  import DocumentationIcon from '@/components/icons/IconDocumentation.vue'
+  import ToolingIcon from '@/components/icons/IconTooling.vue'
+  import EcosystemIcon from '@/components/icons/IconEcosystem.vue'
+  import SupportIcon from '@/components/icons/IconSupport.vue'
 </script>
 
 <style scoped>
