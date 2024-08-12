63df2547442338f18ec3036a574981975ccf45c6
Roger Lo <sobadrush@icloud.com>
2024-08-12 22:15:48 +0800

    chore: Update Component-F.vue to wrap content in a div

--- src/components/Component-F.vue
+++ src/components/Component-F.vue
@@ -1,15 +1,17 @@
 <template>
-  <h1>Component-F (父組件)</h1>
-  <hr/>
-
-  <!-- 
-    "community": IconCommunity,
-    "documentation": IconDocumentation,
-    "ecosystem": IconEcosystem,
-    "support": IconSupport,
-    "tooling": IconTooling 
-  -->
-  <ComponentG :p_iconName="'tooling'"/>
+  <div style="border: 0.3rem dotted orange; padding: 0.5rem;">
+    <h1>Component-F (父組件)</h1>
+    <hr/>
+    
+    <!-- 
+      "community": IconCommunity,
+      "documentation": IconDocumentation,
+      "ecosystem": IconEcosystem,
+      "support": IconSupport,
+      "tooling": IconTooling 
+    -->
+    <ComponentG :p_iconName="'tooling'"/>
+  </div>
 
 </template>
 
