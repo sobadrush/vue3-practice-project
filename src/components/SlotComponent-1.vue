20f321526090c813c34fd819be543236610cca7a
Roger Lo <sobadrush@icloud.com>
2024-08-15 08:34:37 +0800

    測試-具名插槽

--- src/components/SlotComponent-1.vue
+++ src/components/SlotComponent-1.vue
@@ -3,10 +3,24 @@
     <h1 style="color:darkolivegreen">Im SlotComponent-1 (子組件)</h1>
     <hr/>
 
-    <div style="border: 1px solid palevioletred; width: 30%; height: 10rem; margin: 5rem;" >
-      子組件的插槽區(slot)<br/>
+    <div style="border: 1px solid palevioletred; width: 40%; height: 12rem; margin: 3rem;" >
+      子組件的【匿名插槽區】 Anonymous Slot<br/>
       <!-- <slot /> --> <!-- 插槽：類似在子組件挖洞 -->
       <slot><h4 style="color: brown;">我是 slot 預設值</h4></slot>
+      <!-- <slot name="default"><h4 style="color: brown;">我是 slot 預設值</h4></slot> -->
+    </div>
+
+    <div style="border: 1px solid palevioletred; width: 40%; height: 12rem; margin: 3rem;" >
+      子組件的【具名插槽區】Named Slot<br/>
+      <slot name="namedSlot_1">
+        <h4 style="color: burlywood;">我是 namedSlot_1 的預設值</h4>
+      </slot>
+
+      <br/>
+
+      <slot name="namedSlot_2">
+        <h4 style="color: burlywood;">我是 namedSlot_2 預設值</h4>
+      </slot>
     </div>
 
   </div>
--- src/components/SlotTestComponent.vue
+++ src/components/SlotTestComponent.vue
@@ -2,8 +2,19 @@
   <h1 style="color:tomato">Im SlotTestComponent(父組件)</h1>
 
   <SlotComponent1>
-    <!-- <h5 style="background-color: bisque;">Hello From Father</h5>
-    <img src="@/assets/images/Kirby-1.webp" alt="Kirby1" width="100" v-for="index in 3" :key="index"/> -->
+    <h5 style="background-color: bisque;">Hello From Father</h5>
+    <img src="@/assets/images/Kirby-1.webp" alt="Kirby1" width="100" v-for="index in 3" :key="index"/>
+
+    <template v-slot:namedSlot_1>
+      <h5 style="color: cornflowerblue;">具名插槽 namedSlot_1 參數：Apple</h5>
+      <button class="btn btn-sm btn-secondary">Click me 1</button>
+    </template>
+
+    <template #namedSlot_2>
+      <h5 style="color: cornflowerblue;">具名插槽 namedSlot_2 參數：Book</h5>
+      <button class="btn btn-sm btn-secondary">Click me 2</button>
+    </template>
+
   </SlotComponent1>
 
   <br/>
